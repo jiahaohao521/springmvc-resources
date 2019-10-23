e06c70091a0fb7f775c0885b798e97416ef5a1a9
贾昊昊 <565409543@qq,com>
2019-10-23 18:26:51 +0800

    第一次提交--课堂原版

--- src/main/java/com/woniuxy/a_hello/UserController.java
+++ src/main/java/com/woniuxy/a_hello/UserController.java
@@ -6,11 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-// 目前而言， 作为Springmvc中的控制器，必须实现Controller接口
 public class UserController implements Controller {
-	@Override
+
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println("问候springmvc它大爷！！！");
+		System.out.println("锟绞猴拷springmvc锟斤拷锟斤拷爷锟斤拷锟斤拷锟斤拷");
 		
 		ModelAndView mav = new ModelAndView();
 		
